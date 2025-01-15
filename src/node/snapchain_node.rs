846d8764112f9dbb2f7de61662eac0e8ee33daba
Sanjay Raveendran <sanjayprabhu@gmail.com>
2025-01-15 14:19:44 -0800

    Revert "Revert malachite upgrade (#201)"
    This reverts commit ec9785c3518e272cb48a17f2f9ce6725e7cdaaa1.

--- src/node/snapchain_node.rs
+++ src/node/snapchain_node.rs
@@ -15,9 +15,10 @@ use crate::storage::store::stores::Stores;
 use crate::storage::store::BlockStore;
 use crate::storage::trie::merkle_trie;
 use crate::utils::statsd_wrapper::StatsdClientWrapper;
+use informalsystems_malachitebft_config::TimeoutConfig;
+use informalsystems_malachitebft_core_consensus::ValuePayload;
+use informalsystems_malachitebft_metrics::Metrics;
 use libp2p::identity::ed25519::Keypair;
-use malachite_config::TimeoutConfig;
-use malachite_metrics::Metrics;
 use ractor::ActorRef;
 use std::collections::{BTreeMap, HashMap};
 use tokio::sync::{broadcast, mpsc};
@@ -74,10 +75,11 @@ impl SnapchainNode {
             );
             let shard_validator_set = SnapchainValidatorSet::new(vec![shard_validator]);
             let shard_consensus_params = ConsensusParams {
-                start_height: Height::new(shard.shard_id(), 1),
+                initial_height: Height::new(shard.shard_id(), 1),
                 initial_validator_set: shard_validator_set,
                 address: validator_address.clone(),
                 threshold_params: Default::default(),
+                value_payload: ValuePayload::ProposalAndParts,
             };
             let ctx = SnapchainValidatorContext::new(keypair.clone());
 
@@ -143,10 +145,11 @@ impl SnapchainNode {
         let block_validator_set = SnapchainValidatorSet::new(vec![block_validator]);
 
         let block_consensus_params = ConsensusParams {
-            start_height: Height::new(block_shard.shard_id(), 1),
+            initial_height: Height::new(block_shard.shard_id(), 1),
             initial_validator_set: block_validator_set,
             address: validator_address.clone(),
             threshold_params: Default::default(),
+            value_payload: ValuePayload::ProposalAndParts,
         };
 
         let engine = BlockEngine::new(block_store.clone());
