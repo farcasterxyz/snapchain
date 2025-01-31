27a177100f331fb945cb8f543424b22da8603f3b
Aditi Srinivasan <aditiharini@gmail.com>
2025-01-30 20:28:38 -0500

    handle errors gracefully

--- src/storage/db/snapshot.rs
+++ src/storage/db/snapshot.rs
@@ -70,8 +70,20 @@ pub enum SnapshotError {
     #[error(transparent)]
     BuildError(#[from] BuildError),
 
+    #[error(transparent)]
+    ReqwestError(#[from] reqwest::Error),
+
+    #[error(transparent)]
+    SerdeJsonError(#[from] serde_json::Error),
+
     #[error("unable to convert time to date")]
     DateError,
+
+    #[error("unable to find parent directory")]
+    MissingParentDirectory,
+
+    #[error("unable to convert file name to string")]
+    UnableToParseFileName,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -160,11 +172,9 @@ pub async fn download_snapshots(
     snapshot_config: &Config,
     db_dir: String,
     shard_id: u32,
-) {
+) -> Result<(), SnapshotError> {
     let snapshot_dir = snapshot_config.snapshot_download_dir.clone();
-    tokio::fs::create_dir_all(snapshot_dir.clone())
-        .await
-        .unwrap();
+    std::fs::create_dir_all(snapshot_dir.clone())?;
     let metadata_url = format!(
         "{}/{}",
         snapshot_config.snapshot_download_url,
@@ -172,57 +182,57 @@ pub async fn download_snapshots(
     );
     info!("Retrieving metadata from {}", metadata_url);
     let metadata_json = reqwest::get(metadata_url)
-        .await
-        .unwrap()
+        .await?
         .json::<SnapshotMetadata>()
-        .await
-        .unwrap();
+        .await?;
     let base_path = metadata_json.key_base;
     for chunk in metadata_json.chunks {
         let download_path = format!(
             "{}/{}/{}",
             snapshot_config.snapshot_download_url, base_path, chunk
         );
-        let download_response = reqwest::get(download_path).await.unwrap();
+        let download_response = reqwest::get(download_path).await?;
         let mut byte_stream = download_response.bytes_stream();
         let filename = format!("{}/{}", snapshot_dir.clone(), chunk);
         info!("Downloading zipped chunk at to {}", filename);
 
-        let mut file = BufWriter::new(tokio::fs::File::create(filename).await.unwrap());
+        let mut file = BufWriter::new(tokio::fs::File::create(filename).await?);
 
         while let Some(piece) = byte_stream.next().await {
-            file.write_all(&piece.unwrap()).await.unwrap();
+            file.write_all(&piece?).await?;
         }
-        file.flush().await.unwrap();
+        file.flush().await?;
     }
 
-    for entry in std::fs::read_dir(snapshot_dir.clone()).unwrap() {
-        info!(
-            "Unzipping file {}",
-            entry.as_ref().unwrap().path().to_str().unwrap()
-        );
-        let file = std::fs::File::open(entry.unwrap().path()).unwrap();
+    for entry in std::fs::read_dir(snapshot_dir.clone())? {
+        let entry = entry?;
+        info!("Unzipping file {}", entry.path().to_string_lossy());
+        let file = std::fs::File::open(entry.path())?;
         let gz_decoder = GzDecoder::new(file);
         let mut archive = Archive::new(gz_decoder);
-        archive.unpack(&db_dir).unwrap();
+        archive.unpack(&db_dir)?;
 
         // Move contents of subdirectory into parent. The unzipped files are put into a subdirectory
-        for entry in std::fs::read_dir(&db_dir).unwrap() {
-            let entry = entry.unwrap();
+        for entry in std::fs::read_dir(&db_dir)? {
+            let entry = entry?;
             if entry.path().is_dir() {
-                for db_file in std::fs::read_dir(entry.path()).unwrap() {
-                    let db_file = db_file.unwrap();
+                for db_file in std::fs::read_dir(entry.path())? {
+                    let db_file = db_file?;
                     std::fs::rename(
                         db_file.path(),
-                        db_file.path().parent().unwrap().join(db_file.file_name()),
-                    )
-                    .unwrap()
+                        db_file
+                            .path()
+                            .parent()
+                            .ok_or(SnapshotError::MissingParentDirectory)?
+                            .join(db_file.file_name()),
+                    )?
                 }
-                std::fs::remove_dir_all(entry.path()).unwrap();
+                std::fs::remove_dir_all(entry.path())?;
             }
         }
     }
-    std::fs::remove_dir_all(snapshot_dir).unwrap();
+    std::fs::remove_dir_all(snapshot_dir)?;
+    Ok(())
 }
 
 pub async fn upload_to_s3(
@@ -247,7 +257,11 @@ pub async fn upload_to_s3(
     let mut file_names: Vec<String> = vec![];
     for entry in files {
         let entry = entry?;
-        let file_name = entry.file_name().to_str().unwrap().to_string();
+        let file_name = entry
+            .file_name()
+            .to_str()
+            .ok_or(SnapshotError::UnableToParseFileName)?
+            .to_string();
         let key = format!("{}/{}", upload_dir, file_name);
 
         info!(key, "Uploading chunk to s3");
@@ -277,7 +291,7 @@ pub async fn upload_to_s3(
         timestamp: start_timetamp,
     };
 
-    let metadata_json = serde_json::to_string(&metadata).unwrap();
+    let metadata_json = serde_json::to_string(&metadata)?;
     let metadata_key = metadata_path(network, shard_id);
     let upload_result = s3_client
         .put_object()
