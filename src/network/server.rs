a1a7a7e156041fedb89ec6b272ac1bf25f05e1d7
gr4yha7 <jamaal.ekpenyong22@gmail.com>
2025-04-21 22:17:55 +0100

    feat: add /v1/events http API endpoint

--- src/network/server.rs
+++ src/network/server.rs
@@ -39,7 +39,7 @@ use crate::proto::ValidationResponse;
 use crate::proto::VerificationAddAddressBody;
 use crate::proto::{Block, CastId, DbStats};
 use crate::proto::{
-    BlocksRequest, EventRequest, ShardChunksRequest, ShardChunksResponse, SubscribeRequest,
+    BlocksRequest, EventRequest, EventsRequest, EventsResponse, ShardChunksRequest, ShardChunksResponse, SubscribeRequest,
 };
 use crate::proto::{FidRequest, FidTimestampRequest};
 use crate::proto::{GetInfoRequest, StorageLimitsResponse};
@@ -51,7 +51,7 @@ use crate::storage::constants::OnChainEventPostfix;
 use crate::storage::constants::RootPrefix;
 use crate::storage::db::PageOptions;
 use crate::storage::db::RocksDbTransactionBatch;
-use crate::storage::store::account::HubEventIdGenerator;
+use crate::storage::store::account::{EventsPage, HubEventIdGenerator};
 use crate::storage::store::account::MessagesPage;
 use crate::storage::store::account::UsernameProofStore;
 use crate::storage::store::account::{message_bytes_decode, IntoI32};
@@ -813,6 +813,57 @@ impl HubService for MyHubService {
             }
             Err(err) => Err(Status::internal(err.to_string())),
         }
+    }
+
+    async fn get_events(
+        &self,
+        request: Request<EventsRequest>
+    ) -> Result<Response<EventsResponse>, Status> {
+        let req = request.into_inner();
+
+        let num_shards = self.shard_stores.len();
+        let per_shard_tokens: Vec<Option<Vec<u8>>> = if let Some(token_bytes) = req.page_token {
+            serde_json::from_slice(&token_bytes)
+                .map_err(|e| Status::invalid_argument(format!("Invalid page token: {}", e)))?
+        } else {
+            vec![None; num_shards]
+        };
+        if per_shard_tokens.len() != num_shards {
+            return Err(Status::invalid_argument(
+                "Page token does not match number of shards".to_string(),
+            ));
+        }
+        let pages: Vec<EventsPage> = self
+            .shard_stores
+            .iter()
+            .zip(per_shard_tokens.into_iter())
+            .map(|(shard_entry, shard_token)| {
+                let page_options = PageOptions {
+                    page_size: req.page_size.map(|s| s as usize),
+                    page_token: shard_token,
+                    reverse: req.reverse.unwrap_or(false),
+                };
+                return shard_entry.1.get_events(req.start_id, req.stop_id, Some(page_options))
+                    .unwrap_or(EventsPage {
+                        events: vec![],
+                        next_page_token: None,
+                    });
+            })
+            .collect();
+        let combined_events: Vec<HubEvent> = pages
+            .iter()
+            .flat_map(|page| page.events.clone())
+            .collect();
+        let next_page_tokens: Vec<Option<Vec<u8>>> =
+            pages.into_iter().map(|page| page.next_page_token).collect();
+        let new_page_token = serde_json::to_vec(&next_page_tokens)
+            .map_err(|e| Status::internal(format!("Failed to serialize next_page_token: {}", e)))?;
+        let response = EventsResponse {
+            events: combined_events,
+            next_page_token: Some(new_page_token),
+        };
+
+        Ok(Response::new(response))
     }
 
     async fn get_cast(&self, request: Request<CastId>) -> Result<Response<proto::Message>, Status> {
