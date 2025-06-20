c631475c893b114f23a5397877eaeff8f3fa69ac
Aditi Srinivasan <aditiharini@gmail.com>
2025-06-19 22:11:42 -0400

    fix

--- src/storage/store/engine_tests.rs
+++ src/storage/store/engine_tests.rs
@@ -1458,7 +1458,7 @@ mod tests {
             &mut engine,
             &cast_add,
             "bad_request.validation_failure",
-            "Invalid network",
+            "invalid network",
         )
         .await;
     }
