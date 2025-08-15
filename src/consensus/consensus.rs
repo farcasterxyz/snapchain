e330ef0a005a6c77ce9577c4d812d0fea46fd97e
Aditi Srinivasan <aditiharini@gmail.com>
2025-08-14 20:15:18 -0400

    updates

--- src/consensus/consensus.rs
+++ src/consensus/consensus.rs
@@ -41,7 +41,6 @@ pub struct Config {
     pub step_delta: Duration, // Timeout delta between steps
     #[serde(with = "humantime_serde")]
     pub propose_time: Duration, // Timeout for each propose/prevote/precommit step
-    pub shard_witness_proposal_timeout: Duration, // Timeout for waiting for confirmed shard chunks
     #[serde(with = "humantime_serde")]
     pub prevote_time: Duration, // Timeout for each propose/prevote/precommit step
     #[serde(with = "humantime_serde")]
@@ -82,7 +81,6 @@ impl Config {
             consensus_start_delay: self.consensus_start_delay,
             sync_request_timeout: self.sync_request_timeout,
             sync_status_update_interval: self.sync_status_update_interval,
-            shard_witness_proposal_timeout: self.shard_witness_proposal_timeout,
         }
     }
 
@@ -112,7 +110,6 @@ impl Default for Config {
             shard_ids: vec![1],
             num_shards: 1,
             propose_time: Duration::from_millis(1000),
-            shard_witness_proposal_timeout: Duration::from_millis(500),
             prevote_time: Duration::from_millis(500),
             precommit_time: Duration::from_millis(500),
             step_delta: Duration::from_millis(500),
--- src/consensus/proposer.rs
+++ src/consensus/proposer.rs
@@ -338,7 +338,6 @@ pub struct BlockProposer {
     block_tx: Option<mpsc::Sender<Block>>,
     engine: BlockEngine,
     statsd_client: StatsdClientWrapper,
-    shard_witness_proposal_timeout: Duration,
 }
 
 impl BlockProposer {
@@ -350,7 +349,6 @@ impl BlockProposer {
         network: proto::FarcasterNetwork,
         block_tx: Option<mpsc::Sender<Block>>,
         engine: BlockEngine,
-        shard_witness_proposal_timeout: Duration,
         statsd_client: StatsdClientWrapper,
     ) -> BlockProposer {
         BlockProposer {
@@ -363,7 +361,6 @@ impl BlockProposer {
             block_tx,
             engine,
             statsd_client,
-            shard_witness_proposal_timeout,
         }
     }
 
@@ -498,10 +495,8 @@ impl Proposer for BlockProposer {
         let timestamp = FarcasterTime::current();
         let version = EngineVersion::version_for(&timestamp, self.network);
 
-        let shard_witness_timeout = timeout.min(self.shard_witness_proposal_timeout);
-
         let shard_witnesses = self
-            .collect_confirmed_shard_witnesses(height, version, shard_witness_timeout)
+            .collect_confirmed_shard_witnesses(height, version, timeout)
             .await;
 
         let shard_witness = ShardWitness {
