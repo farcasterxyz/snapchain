05b8bf58e19f550b6089bcb51323b87cbc4997fa
adityapk00 <31996805+adityapk00@users.noreply.github.com>
2025-09-18 12:13:41 -0500

    feat: Configure or get (from public IP) announce_rpc_address (#681)
    Allow setting a `announce_rpc_address` in the config and share that out
    in the ContactInfo gossip messages. If not configured, get it from
    public IP

--- src/network/gossip.rs
+++ src/network/gossip.rs
@@ -1,3 +1,4 @@
+use crate::cfg::{DEFAULT_GOSSIP_PORT, DEFAULT_RPC_PORT};
 use crate::consensus::consensus::{MalachiteEventShard, SystemMessage};
 use crate::consensus::malachite::network_connector::MalachiteNetworkEvent;
 use crate::consensus::malachite::snapchain_codec::SnapchainCodec;
@@ -37,7 +38,6 @@ use tokio::sync::mpsc::Sender;
 use tokio::sync::{mpsc, oneshot};
 use tracing::{debug, error, info, warn};
 
-const DEFAULT_GOSSIP_PORT: u16 = 3382;
 const DEFAULT_GOSSIP_HOST: &str = "127.0.0.1";
 const MAX_GOSSIP_MESSAGE_SIZE: usize = 1024 * 1024 * 10; // 10 mb
 
@@ -49,12 +49,26 @@ const CONTACT_INFO: &str = "contact-info";
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Config {
+    // The address to listen on. (eg address="/ip4/0.0.0.0/udp/3382/quic-v1")
     pub address: String,
+    // What address to announce to peers. Useful if behind NAT or public IP is different.
+    // eg announce_address="/ip4/56.23.122.23/udp/3382/quic-v1"
+    // If empty, will try to detect public IP and fall back to `address` if detection fails.
     pub announce_address: String,
+    // What RPC address to announce to peers. Useful if behind NAT or public IP is different.
+    // eg announce_rpc_address="http://56.23.122.23:3381"
+    // or announce_rpc_address="https://mydomain.com:3381" if using a domain with SSL termination
+    pub announce_rpc_address: String,
+    // List of bootstrap peers to connect to on startup, comma-separated.
+    // eg bootstrap_peers = "/ip4/54.236.164.51/udp/3382/quic-v1, /ip4/54.87.204.167/udp/3382/quic-v1, ..."
     pub bootstrap_peers: String,
+    // Interval at which to publish our contact info to the network.
     pub contact_info_interval: Duration,
+    // Interval at which to attempt to reconnect to bootstrap peers if disconnected.
     pub bootstrap_reconnect_interval: Duration,
+    // Whether to enable auto-discovery of peers via contact info messages.
     pub enable_autodiscovery: bool,
+    // Comma-separated list of peer IDs to always connect to directly.
     pub direct_peers: String,
 }
 
@@ -67,6 +81,7 @@ impl Default for Config {
         Config {
             address: address.clone(),
             announce_address: "".to_string(),
+            announce_rpc_address: "".to_string(),
             bootstrap_peers: "".to_string(),
             contact_info_interval: Duration::from_secs(300),
             bootstrap_reconnect_interval: Duration::from_secs(30),
@@ -173,7 +188,8 @@ pub struct SnapchainGossip {
     enable_autodiscovery: bool,
     bootstrap_addrs: HashSet<String>,
     connected_bootstrap_addrs: HashSet<String>,
-    announce_address: String,
+    announce_gossip_address: String,
+    announce_rpc_address: String,
     fc_network: FarcasterNetwork,
     contact_info_interval: Duration,
     bootstrap_reconnect_interval: Duration,
@@ -307,9 +323,19 @@ impl SnapchainGossip {
         // Listen on all assigned port for this id
         swarm.listen_on(config.address.parse()?)?;
 
-        let announce_address = Self::get_announce_address(config).await;
+        let announce_gossip_address = Self::get_announce_gossip_address(fc_network, config).await;
+        let announce_rpc_address = match Self::get_announce_rpc_address(fc_network, config).await {
+            Ok(addr) => addr,
+            Err(e) => {
+                warn!("Failed to get announce RPC address: {}", e);
+                "".to_string()
+            }
+        };
 
-        info!("Using {} as announce address", announce_address);
+        info!(
+            announce_gossip_address,
+            announce_rpc_address, "Configured announce addresses",
+        );
 
         // ~5 seconds of buffer (assuming 1K msgs/pec)
         let (tx, rx) = mpsc::channel(5000);
@@ -321,7 +347,8 @@ impl SnapchainGossip {
             sync_channels: HashMap::new(),
             read_node,
             bootstrap_addrs: config.bootstrap_addrs().into_iter().collect(),
-            announce_address,
+            announce_gossip_address,
+            announce_rpc_address,
             fc_network,
             contact_info_interval: config.contact_info_interval,
             bootstrap_reconnect_interval: config.bootstrap_reconnect_interval,
@@ -332,9 +359,35 @@ impl SnapchainGossip {
         })
     }
 
-    async fn get_announce_address(config: &Config) -> String {
+    async fn get_announce_rpc_address(
+        fc_network: FarcasterNetwork,
+        config: &Config,
+    ) -> Result<String, reqwest::Error> {
+        if config.announce_rpc_address.len() > 0 {
+            return Ok(config.announce_rpc_address.clone());
+        }
+
+        if fc_network == FarcasterNetwork::Devnet {
+            // Don't try to fetch public IP for devnet/during tests
+            return Ok("".to_string());
+        }
+
+        // If no config-defined announce RPC IP exists, detect the public IP.
+        Self::get_public_ip()
+            .await
+            // Use http if using IP address (assumeno SSL)
+            .map(|ip| format!("http://{}:{}", ip, DEFAULT_RPC_PORT))
+    }
+
+    async fn get_announce_gossip_address(fc_network: FarcasterNetwork, config: &Config) -> String {
         if config.announce_address.len() > 0 {
             return config.announce_address.clone();
+        }
+
+        if fc_network == FarcasterNetwork::Devnet {
+            // Don't try to fetch public IP for devnet/during tests
+            // Fallback to address.
+            return config.address.clone();
         }
 
         // If no config-defined announce IP exists, detect the public IP.
@@ -398,7 +451,8 @@ impl SnapchainGossip {
         let contact_info = ContactInfo {
             body: Some(ContactInfoBody {
                 peer_id: self.swarm.local_peer_id().to_bytes(),
-                gossip_address: self.announce_address.clone(),
+                gossip_address: self.announce_gossip_address.clone(),
+                announce_rpc_address: self.announce_rpc_address.clone(),
                 network: self.fc_network as i32,
                 snapchain_version: current_version.to_string(),
                 timestamp: std::time::SystemTime::now()
