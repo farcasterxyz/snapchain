9118187e62115b1f0047d9a25d51d9cbcf12901c
Aditi Srinivasan <aditiharini@gmail.com>
2025-12-19 22:07:18 -0500

    chore: take a replication snapshot if there are none

--- src/network/replication/replicator.rs
+++ src/network/replication/replicator.rs
@@ -743,8 +743,14 @@ impl Replicator {
         self.stores
             .close_aged_snapshots(msg.shard_id, oldest_valid_timestamp);
 
+        // Take a snapshot if none exist because there aren't many read nodes running and we may have to wait a long time for the scheduled snapshot after restart. This snapshot won't be at the same height across all nodes, but will be the first one pruned.
+        let take_first_snapshot = self.stores.max_height_for_shard(msg.shard_id).is_none();
+
         // Check if we can take a snapshot of this block
-        if block_number > 0 && block_number % self.snapshot_options.interval != 0 {
+        if block_number > 0
+            && block_number % self.snapshot_options.interval != 0
+            && !take_first_snapshot
+        {
             return Ok(());
         }
 
